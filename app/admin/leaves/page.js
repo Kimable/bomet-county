30e3b9a56be9ff40ecf2821e53038f5337c02ab0
Kelvin Kimani <kkimani@yahoo.com>
2024-04-10 12:05:06 +0300

    Alerts are fetched in realtime now

--- app/admin/leaves/page.js
+++ app/admin/leaves/page.js
@@ -5,8 +5,8 @@ import BreadCrumb from "@/app/components/common/breadcrumbs/page";
 import { FiCalendar } from "react-icons/fi";
 import LeavesList from "@/app/components/admin/leaves/page";
 import CurrentDate from "@/app/components/common/currentdate/page";
-import Toast from "@/app/cloud/components/Toast";
 import { ToastContainer } from "react-toastify";
+import { showToast } from "@/app/components/toast";
 
 const Leaves = () => {
   const [leaves, setLeaves] = useState([]);
@@ -23,11 +23,11 @@ const Leaves = () => {
       })
       .then((fetchedLeaves) => {
         setLeaves(fetchedLeaves.allLeaves);
-        Toast("Status Updated Successfully");
+        showToast("Status Updated Successfully");
       })
       .catch((error) => {
         console.error("Error fetching leaves:", error);
-        Toast("Status Failed to Update");
+        showToast("Status Failed to Update");
       });
   }
 
